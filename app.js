2d8ec6fa00928e8b32f7906091d6b54947ca9bdf
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-10-08 05:54:38 +0200

    working on signup.html file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 import express from "express";
 import bodyParser from "body-parser";
-import require from "request";
+import request from "request";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
