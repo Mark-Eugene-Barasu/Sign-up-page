e1613d279867364e300b9b8344cbdece15c74e8f
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-10-08 04:52:36 +0200

    setting up boiler plate for NewsLetter SignUp page

---
+++ app.js
@@ -0,0 +1,27 @@
+// jshint ESversion:6
+
+import express from "express";
+import bodyParser from "body-parser";
+// import require from "require";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+    res.send(`Server running Successfully`);
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log(`Server running on port: 3000`)
+})
+
+
