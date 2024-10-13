439d18dd3d2055bef8fe51a87d09e07e1a94d3e2
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-10-13 10:45:38 +0200

    improvements

--- app.js
+++ app.js
@@ -4,24 +4,28 @@ import express from "express";
 import bodyParser from "body-parser";
 import request from "request";
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// to import path and url modules
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.send(`Server running Successfully`);
-})
-
-
-
-
-
-
-
-
-
+    res.sendFile(`${__dirname}/public/signup.html`);
+});
 
 app.listen(3000, function(){
     console.log(`Server running on port: 3000`)
+    console.log(`${__filename}`);
+    console.log(`${__dirname}`); 
 })
 
 
